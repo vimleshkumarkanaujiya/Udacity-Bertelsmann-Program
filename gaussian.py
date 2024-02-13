edcd168e1602db6c4061eaee7e15d41b7a2a498b
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2024-02-14 05:13:40 +0530

    Update gaussian.py

--- gaussian.py
+++ gaussian.py
@@ -15,7 +15,29 @@ class Gaussian(Distribution):
     """
 
     def __init__(self, mu=0, sigma=1): 
-        Distribution.__init__(self, mu, sigma) 
+     Distribution.__init__(self, mu, sigma)
+
+    def read_data_file(self, file_name):
+        """Function to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute.
+
+        Args:
+            file_name (string): name of a file to read from
+
+        Returns:
+            None
+
+        """
+
+        with open(file_name) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+        file.close()
+
+        self.data = data_list
 
     def calculate_mean(self):    
         """Function to calculate the mean of the data set. 
